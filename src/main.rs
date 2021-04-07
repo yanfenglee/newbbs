7872054d7c5b8295a518bf2f3262c92d171648ab
yanfenglee <muxsdt@gmail.com>
2021-04-07 18:25:53 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-//extern crate config;
-//extern crate serde;
 
-#[macro_use]
-extern crate serde_derive;
-
-mod settings;
-
-use settings::Settings;
+use newbbs::settings::Settings;
 
 fn main() {
     let settings = Settings::new();
